031dbb02282df7cb8e9aee56981eea09f04d8d24
Sam Collier <sam@samcollier.tech>
2022-03-18 21:41:35 +0000

    even easier entity adding - thx templates

--- Level.cpp
+++ Level.cpp
@@ -20,36 +20,34 @@ void Level::loadTextures()
 void Level::buildGraph()
 {
 	// Player Entity
-	std::unique_ptr<Node> mario(new Mario(textures,&window));
+	std::unique_ptr<Mario> mario(new Mario(textures,&window));
 	mario->setPosition(spawnPos);
-	addNode(mario,Entity,true);
+	addNode(&mario,Entity,true);
 
 	// Test Wall
 	for(int i=0;i<10;i++)
 	{
-		std::unique_ptr<Node> floorNode(new Actor(textures));
-		Actor* floor = reinterpret_cast<Actor*>(floorNode.get());
+		std::unique_ptr<Actor> floor(new Actor(textures));
 		floor->setTexture(Textures::Brick);
 		floor->setPosition(spawnPos+sf::Vector2f(i*16*5+500.f,0.f));
 		floor->setTextureRect(sf::IntRect(0,0,16,16));
 		floor->setCollisionBox(sf::FloatRect(0,0,16,16));
 		floor->scale(5.f, 5.f);
-		addNode(floorNode,Object,true);
+		addNode(&floor,Object,true);
 
-		std::unique_ptr<Node> wallNode(new Actor(textures));
-		Actor* wall = reinterpret_cast<Actor*>(wallNode.get());
+		std::unique_ptr<Actor> wall(new Actor(textures));
 		wall->setTexture(Textures::Brick);
 		wall->setPosition(spawnPos+sf::Vector2f(16*5*10+500.f,(i)*-16*5));
 		wall->setTextureRect(sf::IntRect(0,0,16,16));
 		wall->setCollisionBox(sf::FloatRect(0,0,16,16));
 		wall->scale(5.f, 5.f);
-		addNode(wallNode,Object,true);
+		addNode(&wall,Object,true);
 	}
 	
 	// bkg
-	std::unique_ptr<Node> bkg(new SpriteNode(textures.get(Textures::Terminal)));
+	std::unique_ptr<SpriteNode> bkg(new SpriteNode(textures.get(Textures::Terminal)));
 	bkg->setPosition(0.f,0.f);
 	bkg->scale(4.f,4.f);
-	addNode(bkg,Background);
+	addNode(&bkg,Background);
 }
 
